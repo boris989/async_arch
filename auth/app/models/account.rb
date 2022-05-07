4ad64dc246101fb9df9ca7f4bf8ae04da511c520
boris_korkmazov <boris.korkmazov@gmail.com>
2022-05-07 13:07:55 +0300

    [HW-2] Добавить отправку сообщения при создании аккаунта

--- auth/app/models/account.rb
+++ auth/app/models/account.rb
@@ -3,4 +3,18 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create do
+    event = {
+      event_name: 'Auth.AccountCreated',
+      data: {
+        public_id: public_id,
+        full_name: full_name,
+        email: email,
+        role: role
+      }
+    }
+
+    WaterDrop::SyncProducer.call(event.to_json, topic: 'accounts-stream')
+  end
 end
