7a271e61cab377aab765ec4dd7878080ab167b55
boris_korkmazov <boris.korkmazov@gmail.com>
2022-05-07 12:41:33 +0300

    [HW-2] Добавить обновление аккаунта

--- auth/app/controllers/accounts_controller.rb
+++ auth/app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
   before_action :authenticate_account!
+  before_action :set_account, only: %i[edit update]
 
   def index
     @accounts = Account.all
@@ -9,6 +10,34 @@ class AccountsController < ApplicationController
   end
 
   def update
+    new_role = @account.role != account_params[:role] ? account_params[:role] : nil
+
+    if @account.update(account_params)
+      event = {
+        event_name: 'Auth.AccountUpdated',
+        data: {
+          public_id: @account.public_id,
+          email: @account.email,
+          full_name: @account.full_name
+        }
+      }
+
+      WaterDrop::SyncProducer.call(event.to_json, topic: 'accounts-stream')
+
+      if new_role
+        event = {
+          event_name: 'Auth.AccountRoleChanged',
+          data: {
+            public_id: @account.public_id,
+            role: @account.role
+          }
+        }
+        WaterDrop::SyncProducer.call(event.to_json, topic: 'accounts')
+      end
+      redirect_to root_path, notice: 'Account was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
